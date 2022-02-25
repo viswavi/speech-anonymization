5588055dac767ed47c30a48c36e3a7763ffacbaf
Akruti Kushwaha <akrutikushwaha@Akrutis-MacBook-Pro.local>
2022-02-25 01:05:00 -0500

    added ConvAE model, left original speechbrain_train file for reference for ASR,

--- speechbrain_convae_train.py
+++ speechbrain_convae_train.py
@@ -3,31 +3,41 @@
 Vijay's comments
 ----
 Running instructions:
+
 python speechbrain_train.py \
     speechbrain/recipes/LibriSpeech/ASR/transformer/hparams/transformer.yaml \
     --device cpu
+
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
+
 Recipe for training a Transformer ASR system with librispeech.
 The system employs an encoder, a decoder, and an attention mechanism
 between them. Decoding is performed with (CTC/Att joint) beamsearch coupled with a neural
 language model.
+
 To run this recipe, do the following:
 > python train.py hparams/transformer.yaml
 > python train.py hparams/conformer.yaml
+
 With the default hyperparameters, the system employs a convolutional frontend and a transformer.
 The decoder is based on a Transformer decoder. Beamsearch coupled with a Transformer
 language model is used  on the top of decoder probabilities.
+
 The neural network is trained on both CTC and negative-log likelihood
 targets and sub-word units estimated with Byte Pairwise Encoding (BPE)
 are used as basic recognition tokens. Training is performed on the full
 LibriSpeech dataset (960 h).
+
 The best model is the average of the checkpoints from last 5 epochs.
+
 The experiment file is flexibple enough to support a large variety of
 different systems. By properly changing the parameter files, you can try
 different encoders, decoders, tokens (e.g, characters instead of BPE),
 training split (e.g, train-clean 100 rather than the full one), and many
 other possible variations.
+
+
 Authors
  * Jianyuan Zhong 2020
  * Mirco Ravanelli 2020
@@ -43,6 +53,7 @@ from pathlib import Path
 import speechbrain as sb
 from hyperpyyaml import load_hyperpyyaml
 from speechbrain.utils.distributed import run_on_main
+from models.ConvAutoEncoder import ConvAutoencoder
 
 logger = logging.getLogger(__name__)
 
@@ -52,7 +63,7 @@ from librispeech_prepare import prepare_librispeech  # noqa
 
 
 # Define training procedure
-class ASR(sb.core.Brain):
+class SexAnonymizationTraining(sb.core.Brain):
     def compute_forward(self, batch, stage):
         """Forward computations from the waveform batches to the output probabilities."""
         batch = batch.to(self.device)
@@ -70,93 +81,58 @@ class ASR(sb.core.Brain):
         # compute features
         feats = self.hparams.compute_features(wavs)
 
-        # TODO(Vijay, Akruti): replace the next 40 lines with our own model architecture
-
         current_epoch = self.hparams.epoch_counter.current
         feats = self.modules.normalize(feats, wav_lens, epoch=current_epoch)
+
+        # need to swap to [ BATCH_SIZE x MFCC_FEATURE_DIM x NUM_TIMESTAMPS ]
+        feats = feats.reshape(self.hparams.batch_size, feats.shape[2], feats.shape[1])
+        # AE model expects %4 sized dimension for proper reconstruction 
+        if feats.shape[2]%4 != 0:
+            feats = torch.nn.functional.pad(input=feats, pad=(0, 4-feats.shape[2]%4, 0, 0, 0, 0,), mode='constant', value=0)
 
         if stage == sb.Stage.TRAIN:
             if hasattr(self.hparams, "augmentation"):
                 feats = self.hparams.augmentation(feats)
 
-        # forward modules
-        src = self.modules.CNN(feats)
-        enc_out, pred = self.modules.Transformer(
-            src, tokens_bos, wav_lens, pad_idx=self.hparams.pad_index
-        )
-
-        # output layer for ctc log-probabilities
-        logits = self.modules.ctc_lin(enc_out)
-        p_ctc = self.hparams.log_softmax(logits)
-
-        # output layer for seq2seq log-probabilities
-        pred = self.modules.seq_lin(pred)
-        p_seq = self.hparams.log_softmax(pred)
-
-        # Compute outputs
-        hyps = None
-        if stage == sb.Stage.TRAIN:
-            hyps = None
-        elif stage == sb.Stage.VALID:
-            hyps = None
-            current_epoch = self.hparams.epoch_counter.current
-            if current_epoch % self.hparams.valid_search_interval == 0:
-                # for the sake of efficiency, we only perform beamsearch with limited capacity
-                # and no LM to give user some idea of how the AM is doing
-                hyps, _ = self.hparams.valid_search(enc_out.detach(), wav_lens)
-        elif stage == sb.Stage.TEST:
-            hyps, _ = self.hparams.test_search(enc_out.detach(), wav_lens)
-
-        return p_ctc, p_seq, wav_lens, hyps
+        # forward pass through the model
+        return self.modules.ConvAE(feats)
 
     def compute_objectives(self, predictions, batch, stage):
-        """Computes the loss (CTC+NLL) given predictions and targets."""
+        """Forward computations from the waveform batches to the output probabilities."""
+        reconstructed_speech, sex_logits  = predictions
 
-        (p_ctc, p_seq, wav_lens, hyps,) = predictions
+        sex_label = batch.gender
+        wavs, wav_lens = batch.sig
 
-        ids = batch.id
-        tokens_eos, tokens_eos_lens = batch.tokens_eos
-        tokens, tokens_lens = batch.tokens
+        # compute features
+        feats = self.hparams.compute_features(wavs)
 
-        if hasattr(self.modules, "env_corrupt") and stage == sb.Stage.TRAIN:
-            tokens_eos = torch.cat([tokens_eos, tokens_eos], dim=0)
-            tokens_eos_lens = torch.cat(
-                [tokens_eos_lens, tokens_eos_lens], dim=0
-            )
-            tokens = torch.cat([tokens, tokens], dim=0)
-            tokens_lens = torch.cat([tokens_lens, tokens_lens], dim=0)
+        # need to swap to [ BATCH_SIZE x MFCC_FEATURE_DIM x NUM_TIMESTAMPS ]
+        feats = feats.reshape(self.hparams.batch_size, feats.shape[2], feats.shape[1])
+        # AE model expects %4 sized dimension for proper reconstruction 
+        if feats.shape[2]%4 != 0:
+            feats = torch.nn.functional.pad(input=feats, pad=(0, 4-feats.shape[2]%4, 0, 0, 0, 0,), mode='constant', value=0)
 
-        loss_seq = self.hparams.seq_cost(
-            p_seq, tokens_eos, length=tokens_eos_lens
-        )
-        loss_ctc = self.hparams.ctc_cost(p_ctc, tokens, wav_lens, tokens_lens)
+        recon_loss = self.hparams.loss_reconstruction(reconstructed_speech, feats)
+        sex_loss = self.hparams.loss_sex_classification(sex_logits, torch.tensor(sex_label))
+
         loss = (
-            self.hparams.ctc_weight * loss_ctc
-            + (1 - self.hparams.ctc_weight) * loss_seq
+            self.hparams.recon_loss_weight * recon_loss
+            + self.hparams.sex_loss_weight * sex_loss
         )
 
         if stage != sb.Stage.TRAIN:
             current_epoch = self.hparams.epoch_counter.current
-            valid_search_interval = self.hparams.valid_search_interval
-            if current_epoch % valid_search_interval == 0 or (
-                stage == sb.Stage.TEST
-            ):
-                # Decode token terms to words
-                predicted_words = [
-                    tokenizer.decode_ids(utt_seq).split(" ") for utt_seq in hyps
-                ]
-                target_words = [wrd.split(" ") for wrd in batch.wrd]
-                self.wer_metric.append(ids, predicted_words, target_words)
+            # compute the accuracy of the sex prediction
+            self.acc_metric.append(sex_logits, sex_label, torch.tensor(sex_label.shape[0]))
 
-            # compute the accuracy of the one-step-forward prediction
-            self.acc_metric.append(p_seq, tokens_eos, tokens_eos_lens)
         return loss
 
     def fit_batch(self, batch):
         """Train the parameters given a single batch in input"""
         # check if we need to switch optimizer
         # if so change the optimizer from Adam to SGD
-        self.check_and_reset_optimizer()
+        # self.check_and_reset_optimizer()
 
         predictions = self.compute_forward(batch, sb.Stage.TRAIN)
         loss = self.compute_objectives(predictions, batch, sb.Stage.TRAIN)
@@ -186,8 +162,7 @@ class ASR(sb.core.Brain):
     def on_stage_start(self, stage, epoch):
         """Gets called at the beginning of each epoch"""
         if stage != sb.Stage.TRAIN:
-            self.acc_metric = self.hparams.acc_computer()
-            self.wer_metric = self.hparams.error_rate_computer()
+            self.sex_classification_acc = self.hparams.sex_classification_acc()
 
     def on_stage_end(self, stage, stage_loss, epoch):
         """Gets called at the end of a epoch."""
@@ -196,15 +171,9 @@ class ASR(sb.core.Brain):
         if stage == sb.Stage.TRAIN:
             self.train_stats = stage_stats
         else:
-            stage_stats["ACC"] = self.acc_metric.summarize()
+            stage_stats["ACC"] = self.sex_classification_acc.summarize()
             current_epoch = self.hparams.epoch_counter.current
-            valid_search_interval = self.hparams.valid_search_interval
-            if (
-                current_epoch % valid_search_interval == 0
-                or stage == sb.Stage.TEST
-            ):
-                stage_stats["WER"] = self.wer_metric.summarize("error_rate")
-
+            
         # log stats and save checkpoint at end-of-epoch
         if stage == sb.Stage.VALID and sb.utils.distributed.if_main_process():
 
@@ -241,8 +210,8 @@ class ASR(sb.core.Brain):
                 stats_meta={"Epoch loaded": self.hparams.epoch_counter.current},
                 test_stats=stage_stats,
             )
-            with open(self.hparams.wer_file, "w") as w:
-                self.wer_metric.write_stats(w)
+            # with open(self.hparams.wer_file, "w") as w:
+            #     self.wer_metric.write_stats(w)
 
             # save the averaged checkpoint at the end of the evaluation stage
             # delete the rest of the intermediate checkpoints
@@ -355,6 +324,16 @@ def dataio_prepare(hparams):
             sort_key="duration"
         )
 
+    # converting to binary class labels for easy consumption
+    sex_string_to_int = {'M':0, 'F':1}
+    for item in train_data.data.items():
+        item[1]['gender'] = sex_string_to_int[item[1]['gender']]
+    for item in valid_data.data.items():
+        item[1]['gender'] = sex_string_to_int[item[1]['gender']]
+    for testset in test_datasets:
+        for item in test_datasets[testset].data.items():
+            item[1]['gender'] = sex_string_to_int[item[1]['gender']]
+
     datasets = [train_data, valid_data] + [i for k, i in test_datasets.items()]
 
     # We get the tokenizer as we need it to encode the labels when creating
@@ -430,13 +409,15 @@ if __name__ == "__main__":
     # here we create the datasets objects as well as tokenization and encoding
     train_data, valid_data, test_datasets, tokenizer = dataio_prepare(hparams)
 
+    model = ConvAutoencoder(hparams["convae_feature_dim"])
+
     # We download the pretrained LM from HuggingFace (or elsewhere depending on
     # the path given in the YAML file). The tokenizer is loaded at the same time.
-    run_on_main(hparams["pretrainer"].collect_files)
-    hparams["pretrainer"].load_collected(device=run_opts["device"])
+    #run_on_main(hparams["pretrainer"].collect_files)
+    #hparams["pretrainer"].load_collected(device=run_opts["device"])
 
     # Trainer initialization
-    asr_brain = ASR(
+    sa_brain = SexAnonymizationTraining(
         modules=hparams["modules"],
         opt_class=hparams["Adam"],
         hparams=hparams,
@@ -444,12 +425,13 @@ if __name__ == "__main__":
         checkpointer=hparams["checkpointer"],
     )
 
-    # adding objects to trainer:
-    asr_brain.tokenizer = hparams["tokenizer"]
+    sa_brain.modules['ConvAE'] = model
+
+    hparams["model"].append(sa_brain.modules['ConvAE'])
 
     # Training
-    asr_brain.fit(
-        asr_brain.hparams.epoch_counter,
+    sa_brain.fit(
+        sa_brain.hparams.epoch_counter,
         train_data,
         valid_data,
         train_loader_kwargs=hparams["train_dataloader_opts"],
@@ -458,10 +440,10 @@ if __name__ == "__main__":
 
     # Testing
     for k in test_datasets.keys():  # keys are test_clean, test_other etc
-        asr_brain.hparams.wer_file = os.path.join(
+        sa_brain.hparams.wer_file = os.path.join(
             hparams["output_folder"], "wer_{}.txt".format(k)
         )
-        asr_brain.evaluate(
+        sa_brain.evaluate(
             test_datasets[k],
             max_key="ACC",
             test_loader_kwargs=hparams["test_dataloader_opts"],
