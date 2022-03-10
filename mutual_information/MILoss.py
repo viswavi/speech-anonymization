77cce1c0d5d1fa76a29d8e63be63e48170b286e4
gn03249822 <dah0417912@gmail.com>
2022-03-09 20:24:40 -0500

    incorporated MI loss into training pipeline

--- mutual_information/MILoss.py
+++ mutual_information/MILoss.py
@@ -7,12 +7,13 @@ from .GroupSamplingMI import *
 expecting class-balanced input by batch
 and target/labels by batch
 """
-class MILoss(object):
-    def forward(self, input, target, n_classes=2):
-        # balanced_input, balanced_target, batch_size = _class_balanced_sampling_by_batch(input, target)
-        batch_size = input.shape[0]
-
+class MILoss(nn.Module):
+    """
+    X: discrete variable
+    y: continuous variable
+    """
+    def forward(self, X, y, batch, n_classes=2):
+        batch_size = batch.shape[0]
         group_sampling_MI = GroupSamplingMI(n_samples=batch_size, n_classes=n_classes)
-
-        mi_loss, mi_mean, mi_stdd = group_sampling_MI(input, target, )
+        mi_loss, mi_mean, mi_stdd = group_sampling_MI(X, y, batch)
         return mi_loss
