6ac6bb9f824cacb6f90a9ba1a259b4a2fed40959
gn03249822 <dah0417912@gmail.com>
2022-03-31 10:32:57 -0400

    temp pretrained path

--- speechbrain_convae_train.py
+++ speechbrain_convae_train.py
@@ -468,10 +468,10 @@ if __name__ == "__main__":
         run_opts=run_opts,
         )
     sa_brain.asr_brain.tokenizer = hparams["tokenizer"]
-    sa_brain.asr_brain.tokenizer.Load("pretrained_models/asr-transformer-transformerlm-librispeech/tokenizer.ckpt")
-    hparams["asr_model"].load_state_dict(torch.load("pretrained_models/asr-transformer-transformerlm-librispeech/asr.ckpt"))
+    sa_brain.asr_brain.tokenizer.Load("pretrained_ASR/tokenizer.ckpt")
+    hparams["asr_model"].load_state_dict(torch.load("pretrained_ASR/asr.ckpt"))
     #hparams["normalize"].load_state_dict(torch.load("pretrained_models/asr-transformer-transformerlm-librispeech/normalizer.ckpt"))
-    hparams["lm_model"].load_state_dict(torch.load("pretrained_models/asr-transformer-transformerlm-librispeech/lm.ckpt"))
+    hparams["lm_model"].load_state_dict(torch.load("pretrained_ASR/lm.ckpt"))
 
     print("done loading")
     # #Training
