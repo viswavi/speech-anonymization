e7597614ec39e026a0d868a3217e57804fc40681
gn03249822 <dah0417912@gmail.com>
2022-04-13 20:10:08 -0700

    separate yaml

--- speechbrain_convae_train.py
+++ speechbrain_convae_train.py
@@ -164,8 +164,9 @@ class SexAnonymizationTraining(sb.core.Brain):
 
     def external_classifier(self):
         classifier = EncoderClassifier.from_hparams(
-            source=self.hparams,
-            savedir=self.hparams.save_folder,
+            source="/home/ec2-user/capstone/speech-anonymization/results/gender_classifier/1230/save/",
+            hparams_file="speechbrain_configs/evaluator_inference.yaml",
+            savedir="/home/ec2-user/capstone/speech-anonymization/results/gender_classifier/1230/save/",
         )
 
         return classifier
