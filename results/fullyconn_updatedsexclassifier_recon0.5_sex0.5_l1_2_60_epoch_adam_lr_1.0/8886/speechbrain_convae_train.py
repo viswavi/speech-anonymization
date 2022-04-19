d6bb82231e7e17931c4a9759b48a80f3486c4f1f
EC2 Default User <ec2-user@ip-172-31-31-230.ec2.internal>
2022-04-19 18:37:50 +0000

    latest gender ckpt

--- results/fullyconn_updatedsexclassifier_recon0.5_sex0.5_l1_2_60_epoch_adam_lr_1.0/8886/speechbrain_convae_train.py
+++ results/fullyconn_updatedsexclassifier_recon0.5_sex0.5_l1_2_60_epoch_adam_lr_1.0/8886/speechbrain_convae_train.py
@@ -102,26 +102,25 @@ class SexAnonymizationTraining(sb.core.Brain):
             # Evaluation: performing classification by externally trained sex classifier
             recon_speech_feats = reconstructed_speech.to(sa_brain.device)
 
-            print("sanity check to make sure external classifier works")
-            sex_logits_extern_orig, score_orig, index_orig, _ = self.external_classifier.classify_batch(wavs.to(sa_brain.device),
-                                                                                                           wav_lens.to(sa_brain.device))
+            with torch.no_grad():
+                sex_logits_extern_orig, score_orig, index_orig, _ = self.external_classifier.classify_batch(wavs.to(sa_brain.device),
+                                                                                                          wav_lens.to(sa_brain.device))
 
-            print("sex logits with external + original raw wav data")
-            print(sex_logits_extern_orig)
+            self.sex_classification_acc_extern_orig.append(sex_logits_extern_orig.unsqueeze(0), sex_label.unsqueeze(0),
+                                                      torch.tensor(sex_label.shape[0],
+                                                                   device=sex_logits_extern_orig.device).unsqueeze(0))
 
-            #sex_logits_extern, score, index = self.external_classifier.classify_batch_feats(recon_speech_feats)
+            with torch.no_grad():
+                sex_logits_extern, score, index = self.external_classifier.classify_batch_feats(recon_speech_feats)
 
-            print("sex logits internal: ")
-            print(sex_logits)
- #           print("sex logits extern: ")
-#            print(sex_logits_extern)
-
-            self.sex_classification_acc_extern.append(sex_logits_extern_orig.unsqueeze(0), sex_label.unsqueeze(0),
-                                               torch.tensor(sex_label.shape[0], device=sex_logits_extern_orig.device).unsqueeze(0))
-
-            print("internal classification ACC = ")
+            self.sex_classification_acc_extern.append(sex_logits_extern.unsqueeze(0), sex_label.unsqueeze(0),
+                                               torch.tensor(sex_label.shape[0], device=sex_logits_extern.device).unsqueeze(0))
+            
+            print("internal classification (w/ GRL) ACC = ")
             print(self.sex_classification_acc.summarize())
-            print("external classification ACC on original wav input = ")
+            print("external classification ACC on original raw wav inputs = ")
+            print(self.sex_classification_acc_extern_orig.summarize())
+            print("external classification ACC on reconstructed feats = ")
             print(self.sex_classification_acc_extern.summarize())
 
             if self.hparams.model_type == "convae":
@@ -204,7 +203,8 @@ class SexAnonymizationTraining(sb.core.Brain):
             else:
                 self.recon_loss.append([])
             self.sex_classification_acc = self.hparams.sex_classification_acc()
-            self.sex_classification_acc_extern = self.hparams.sex_classification_acc_extern()
+            self.sex_classification_acc_extern = self.hparams.sex_classification_acc()
+            self.sex_classification_acc_extern_orig = self.hparams.sex_classification_acc()
             self.utility_similarity_aggregator = self.hparams.utility_similarity_aggregator()
 
             if stage == sb.Stage.TEST:
@@ -504,7 +504,7 @@ if __name__ == "__main__":
 
     print("done loading")
 
-    # # #Training
+    # # # #Training
     # sa_brain.fit(
     #     sa_brain.hparams.epoch_counter,
     #     train_data,
