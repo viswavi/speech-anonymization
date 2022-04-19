b1e005e3f72c1bf078d6a026ff4a320596cbe0b7
gn03249822 <dah0417912@gmail.com>
2022-04-18 17:52:25 -0700

    fix path

--- speechbrain_convae_train.py
+++ speechbrain_convae_train.py
@@ -181,7 +181,7 @@ class SexAnonymizationTraining(sb.core.Brain):
 
     def load_external_classifier(self):
         classifier = EncoderClassifier.from_hparams(
-            source="/home/ec2-user/capstone/speech-anonymization/speechbrain_configs/",
+            source="/home/ec2-user/capstone/speech-anonymization/results/gender_classifier/1230/save/",
             hparams_file="evaluator_inference.yaml",
             savedir="/home/ec2-user/capstone/speech-anonymization/results/gender_classifier/1230/save/",
         )
