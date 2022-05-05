6ca3039b81e5ae4632714ad7722fd174dc628386
gn03249822 <dah0417912@gmail.com>
2022-05-05 06:59:48 -0400

    fc model size

--- model_size_calc.py
+++ model_size_calc.py
@@ -1,36 +1,26 @@
 from models.ConvAutoEncoder import ConvAutoencoder, CycleGANGenerator
-from models.FullyConnected import DummyFullyConnectedAutoencoder, FullyConnectedAutoencoder
+from models.FullyConnected import FullyConnectedAutoencoder
 from models.EndToEnd import ConvReconstruction
 
 import torch
 from torchsummary import summary
 
 
+
+model_fcae = FullyConnectedAutoencoder.to('cpu')
 model_convae = ConvAutoencoder().to('cpu')
 
-param_size = 0
-for param in model_convae.parameters():
-    param_size += param.nelement() * param.element_size()
-buffer_size = 0
-for buffer in model_convae.buffers():
-    buffer_size += buffer.nelement() * buffer.element_size()
 
-size_all_mb = (param_size + buffer_size) / 1024**2
-print('model: Convolutional AutoEncoder size: {:.3f}MB'.format(size_all_mb))
+
+summary(model_fcae, (1, 80*10), device='cpu')
 
 
 summary(model_convae,  (1, 80*10), device='cpu')
 
 model_e2e = ConvReconstruction().to('cpu')
 
-param_size = 0
-for param in model_e2e.parameters():
-    param_size += param.nelement() * param.element_size()
-buffer_size = 0
-for buffer in model_e2e.buffers():
-    buffer_size += buffer.nelement() * buffer.element_size()
 
-size_all_mb = (param_size + buffer_size) / 1024**2
-print('model: Gated Conv AutoEncoder size: {:.3f}MB'.format(size_all_mb))
+
+
 
 summary(model_e2e,  (1, 80*10), device='cpu')
