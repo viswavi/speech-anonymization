94c53379322f1b9c3ed4e813a3684651189b4a46
gn03249822 <dah0417912@gmail.com>
2022-05-04 23:00:58 -0400

    more calc

--- model_size_calc.py
+++ model_size_calc.py
@@ -16,5 +16,16 @@ for buffer in model_convae.buffers():
 
 size_all_mb = (param_size + buffer_size) / 1024**2
 print('model: Convolutional AutoEncoder size: {:.3f}MB'.format(size_all_mb))
+
+
 model_e2e = ConvReconstruction()
 
+param_size = 0
+for param in model_e2e.parameters():
+    param_size += param.nelement() * param.element_size()
+buffer_size = 0
+for buffer in model_e2e.buffers():
+    buffer_size += buffer.nelement() * buffer.element_size()
+
+size_all_mb = (param_size + buffer_size) / 1024**2
+print('model: Gated Conv AutoEncoder size: {:.3f}MB'.format(size_all_mb))
