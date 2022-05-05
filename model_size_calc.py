ef507735ee02f1528639b2dfe625ba4d8f53036c
gn03249822 <dah0417912@gmail.com>
2022-05-05 06:55:01 -0400

    fix device

--- model_size_calc.py
+++ model_size_calc.py
@@ -19,7 +19,7 @@ size_all_mb = (param_size + buffer_size) / 1024**2
 print('model: Convolutional AutoEncoder size: {:.3f}MB'.format(size_all_mb))
 
 
-summary(model_convae,  (1, 80*10), device=torch.device('cuda'))
+summary(model_convae,  (1, 80*10), device='cuda')
 
 model_e2e = ConvReconstruction()
 
@@ -33,4 +33,4 @@ for buffer in model_e2e.buffers():
 size_all_mb = (param_size + buffer_size) / 1024**2
 print('model: Gated Conv AutoEncoder size: {:.3f}MB'.format(size_all_mb))
 
-summary(model_e2e,  (1, 80*10), device=torch.device('cuda'))
+summary(model_e2e,  (1, 80*10), device='cuda')
