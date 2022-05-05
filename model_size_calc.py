879621398aa01e971ca4e938874f376776b40a45
gn03249822 <dah0417912@gmail.com>
2022-05-05 06:55:49 -0400

    all on cpu

--- model_size_calc.py
+++ model_size_calc.py
@@ -19,9 +19,9 @@ size_all_mb = (param_size + buffer_size) / 1024**2
 print('model: Convolutional AutoEncoder size: {:.3f}MB'.format(size_all_mb))
 
 
-summary(model_convae,  (1, 80*10), device='cuda')
+summary(model_convae,  (1, 80*10), device='cpu')
 
-model_e2e = ConvReconstruction()
+model_e2e = ConvReconstruction().to('cpu')
 
 param_size = 0
 for param in model_e2e.parameters():
@@ -33,4 +33,4 @@ for buffer in model_e2e.buffers():
 size_all_mb = (param_size + buffer_size) / 1024**2
 print('model: Gated Conv AutoEncoder size: {:.3f}MB'.format(size_all_mb))
 
-summary(model_e2e,  (1, 80*10), device='cuda')
+summary(model_e2e,  (1, 80*10), device='cpu')
